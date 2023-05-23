cff4d8a8d1881e9309d9e9e903edbd671a950d19
iDebonair <ofemiowolabi@gmail.com>
2023-05-23 10:50:53 +0000

    Initial commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {return true
+       
     }
 }
+
+const verticalJoin = letters[0].map((_, colIndex) =>
+letters.map(row => row[colIndex]).join('')
+);
+
+for (const column of verticalJoin) {
+if (column.includes(word)) {
+  return true;
+}
+}
+
+return false;
+};
 
 module.exports = wordSearch
